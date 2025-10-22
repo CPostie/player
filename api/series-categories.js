5054b28471e86e15c07b0a2999802b731b1cde42
CPostie <github.joystick032@passmail.com>
2025-10-22 18:41:35 +0100

    Add handler for fetching series categories

---
+++ api/series-categories.js
@@ -0,0 +1,12 @@
+import { XTREAM_BASE } from "./config.js";
+
+export default async function handler(req) {
+  const sp = new URL(req.url).searchParams;
+  const username = sp.get("username");
+  const password = sp.get("password");
+
+  const url = `${XTREAM_BASE}/player_api.php?username=${username}&password=${password}&action=get_series_categories`;
+  const r = await fetch(url);
+  const data = await r.json();
+  return new Response(JSON.stringify(data), { headers: { "Content-Type": "application/json" } });
+}
